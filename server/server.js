eb3860eb638cf387edb149c6364ed5db7341e2dd
Your Name <your-email@example.com>
2026-02-19 14:05:02 -0800

    create an endpoint for the route

--- server/server.js
+++ server/server.js
@@ -8,6 +8,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// creates an endpoint for the route "/""
+app.get('/', (req, res) => {
+  res.json({ message: 'Hola, from My template Express' });
+});
+
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Backend running on port ${port}`);
