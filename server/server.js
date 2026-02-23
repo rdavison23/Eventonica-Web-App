a460fbbde45104a9686485876d19cb8edb916cb9
Your Name <your-email@example.com>
2026-02-22 21:48:58 -0800

    update event app.put

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { pool } from './db.js';
 
 dotenv.config();
 
@@ -8,15 +9,83 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// creates an endpoint for the route "/""
+// root route
 app.get('/', (req, res) => {
   res.json({ message: 'Hola, from My template Express' });
 });
 
+// get all events
 app.get('/events', async (req, res) => {
   try {
-    const result = await db.query('SELECT * FROM events ORDER BY id,title');
+    const result = await pool.query('SELECT * FROM events ORDER BY id, title');
     res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+// get one event
+app.get('/events/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query('SELECT * FROM events WHERE id = $1', [id]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'event not found' });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+// create event
+app.post('/events', async (req, res) => {
+  const { title, date, description, is_favorite } = req.body;
+
+  if (!title || !date) {
+    return res.status(400).json({ error: 'title and date required' });
+  }
+
+  try {
+    const result = await pool.query(
+      'INSERT INTO events (title, date, description, is_favorite) VALUES ($1, $2, $3, $4) RETURNING *',
+      [title, date, description || null, is_favorite || false]
+    );
+
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'server error' });
+  }
+});
+
+// UPDATE event
+app.put('/events/:id', async (req, res) => {
+  const { id } = req.params;
+  const { title, date, description, is_favorite } = req.body;
+
+  if (!title || !date) {
+    return res.status(400).json({ error: 'Title and date are required' });
+  }
+
+  try {
+    const result = await pool.query(
+      `UPDATE events 
+         SET title = $1, date = $2, description = $3, is_favorite = $4
+         WHERE id = $5 
+         RETURNING *`,
+      [title, date, description || null, is_favorite ?? false, id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Event not found' });
+    }
+
+    res.json(result.rows[0]);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Server error' });
