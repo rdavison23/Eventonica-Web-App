462edb5918f6abb3d200a85f77e020e4d5309387
Your Name <your-email@example.com>
2026-02-26 18:46:39 -0800

    Add Home page and set up React Router for navigation

---
+++ client/src/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      {' '}
+      <h1>Welcome!</h1> <p>This is the home page.</p>{' '}
+      <a href="/events">Go to Events</a>{' '}
+    </div>
+  );
+}
